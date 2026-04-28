a85ebc4db38925056d239fbeeea037456c64ac1d
Darth-Skywalker <ravinder.sheoran@gmail.com>
2026-04-28 19:33:10 +0100

    Increase timeout from 30s to 50s

--- src/index.js
+++ src/index.js
@@ -129,7 +129,7 @@ async function sendToRasa(chatId, sender, textMessage) {
       sender,
       message: textMessage,
     },
-    { timeout: 30_000 }
+    { timeout: 50_000 }
   );
   return resp.data;
 }
